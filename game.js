554ea190015b332383274070047818effce9dade
dnxClub <si.distribution@hotmail.fr>
2018-12-18 10:35:47 +0100

    Ajout des fichiers

---
+++ game.js
@@ -0,0 +1,25 @@
+
+
+function balader() {
+
+        document.querySelector('.tagagotch').style.backgroundImage = "url('balader1.gif')"
+
+}
+
+function caca() {
+    document.querySelector('.tagagotch').style.backgroundImage = "url('caca.gif')"
+
+}
+
+
+function dormir() {
+    document.querySelector('.tagagotch').style.backgroundImage = "url('dormir.gif')"
+}
+
+
+
+
+
+
+
+
